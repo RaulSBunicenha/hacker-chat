0782082dbc32a424569f95d211f069ade7d39ac4
Raul Bunicenha <raulbunicenha@gmail.com>
2021-03-22 14:31:46 -0300

    Construct Client with Blessed

--- client/src/terminalController.js
+++ client/src/terminalController.js
@@ -1,7 +1,43 @@
+import ComponentsBuilder from './components.js'
+
+
 export default class TerminalController {
     constructor () {}
 
+    #onInputReceived (eventEmitter) {
+        return function () {
+            const message = this.getValue()
+            console.log(message)
+            this.clearValue()
+        }
+    }
+
+    #onMessageReceived ({ screen, chat }) {
+        return msg => {
+            const { userName, message } = msg
+            chat.addItem(`{bold}${userName}{/bold}: ${message}`)
+            screen.render()
+        }
+    }
+
+    #registerEvents (eventEmitter, components) {
+        eventEmitter.on('message:received', this.#onMessageReceived(components))
+    }
+
     async initializeTable (eventEmitter) {
-        console.log('started')
+        const components = new ComponentsBuilder()
+            .setScreen({ title: 'Hacker Chat - Erick Wendel' })
+            .setLayoutComponent()
+            .setInputComponent(this.#onInputReceived(eventEmitter))
+            .setChatComponent()
+            .build()
+
+        this.#registerEvents(eventEmitter, components)
+        components.input.focus()
+        components.screen.render()
+
+        setInterval(() => {
+            eventEmitter.emit('message:received', { userName: 'Raul', message: 'Hello World' })
+        }, 2000)
     }
 }
