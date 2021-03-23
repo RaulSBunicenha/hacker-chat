612d86265af1ddf99ad309c829a3e6f227d2f95f
Raul Bunicenha <raulbunicenha@gmail.com>
2021-03-23 18:30:24 -0300

    Client connection with Server

--- client/index.js
+++ client/index.js
@@ -1,11 +1,14 @@
 import Events from 'events'
 import CliConfig from './src/cliConfig.js'
+import SocketClient from './src/socket.js'
 import TerminalController from './src/terminalController.js'
 
 const [ nodePath, filePah, ...commands ] = process.argv
 const config = CliConfig.parseArguments(commands)
 
 const componentEmitter = new Events()
+const socketClient = new SocketClient(config)
+await socketClient.initialize()
 
 const controller = new TerminalController()
 await controller.initializeTable(componentEmitter)
--- server/index.js
+++ server/index.js
@@ -13,39 +13,3 @@ const controller = new Controller({ socketServer })
 eventEmitter.on(constants.event.NEW_USER_CONNECTED, controller.onNewConnection.bind(controller))
 
 console.log('socket server is running at', server.address().port)
-
-/*eventEmitter.on(constants.event.NEW_USER_CONNECTED, (socket) => {
-    console.log('new connection:', socket.id)
-    
-    socket.on('data', data => {
-        console.log('server received', data.toString())
-        socket.write('world')
-    })
-})
-
-async function testServer () {
-    const options = {
-        port: 9898,
-        host: 'localhost',
-        headers: {
-            Connection: 'Upgrade',
-            Upgrade: 'websocket'
-        }
-    }
-
-    const http = await import('http')
-    const req = http.request(options)
-    req.end()
-
-    req.on('upgrade', (res, socket) => {
-        socket.on('data', data => {
-            console.log('client received', data.toString())
-        })
-
-        setTimeout(() => {
-            socket.write('hello')
-            console.log(); console.log(); console.log();
-        }, 1000)
-    })
-}
-testServer()*/
