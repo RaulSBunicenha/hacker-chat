34c384d506c21da848712659afc65629e81e26b8
Raul Bunicenha <raulbunicenha@gmail.com>
2021-03-23 18:21:09 -0300

    Server Controller Created

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import Event from 'events'
 import { constants } from './src/constants.js'
+import Controller from './src/controller.js'
 import SocketServer from './src/socket.js'
 
 const eventEmitter = new Event()
@@ -8,9 +9,12 @@ const port = process.env.PORT || 9898
 const socketServer = new SocketServer({ port })
 const server = await socketServer.initialize(eventEmitter)
 
+const controller = new Controller({ socketServer })
+eventEmitter.on(constants.event.NEW_USER_CONNECTED, controller.onNewConnection.bind(controller))
+
 console.log('socket server is running at', server.address().port)
 
-eventEmitter.on(constants.event.NEW_USER_CONNECTED, (socket) => {
+/*eventEmitter.on(constants.event.NEW_USER_CONNECTED, (socket) => {
     console.log('new connection:', socket.id)
     
     socket.on('data', data => {
@@ -44,4 +48,4 @@ async function testServer () {
         }, 1000)
     })
 }
-testServer()
+testServer()*/
