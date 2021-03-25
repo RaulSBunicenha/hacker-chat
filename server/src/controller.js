5a522535e4757e58287cb10f07b6b73fbe7bfdfc
Raul Bunicenha <raulbunicenha@gmail.com>
2021-03-25 15:58:10 -0300

    Disconnect User

--- server/src/controller.js
+++ server/src/controller.js
@@ -56,9 +56,26 @@ export default class Controller {
         return usersOnRoom
     }
 
+    #logoutUser (id, roomId) {
+        this.#users.delete(id)
+        const usersOnRoom = this.#rooms.get(roomId)
+        usersOnRoom.delete(id)
+
+        this.#rooms.set(roomId, usersOnRoom)
+    }
+
     #onSocketClosed (id) {
-        return data => {
-            console.log('onSocketClosed', id)
+        return _ => {
+            const { userName, roomId } = this.#users.get(id)
+            console.log(userName, 'disconnected', id)
+            this.#logoutUser(id, roomId)
+
+            this.broadCast({
+                roomId,
+                event: constants.event.DISCONNECT_USER,
+                socketId: id,
+                message: { id, userName }
+            })
         }
     }
 
